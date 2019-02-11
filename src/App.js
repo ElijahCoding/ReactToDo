a73ef48593183c9bb0c2935089290e87f0a36dcb
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-11 18:28:20 +0300

    statically building todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Header from './components/layout/Header'
-import Todos from './components/Todos';
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 import './App.css';
 
@@ -41,14 +42,26 @@ class App extends Component {
         this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
     }
 
+    addTodo = (title) => {
+        const newTodo = {
+            id: 4,
+            title,
+            completed: false
+        }
+        this.setState({ todos: [...this.state.todos, newTodo] })
+    }
+
   render() {
       return (
           <div className="App">
-              <Header></Header>
-              <Todos todos={this.state.todos}
-                     markComplete={ this.markComplete }
-                     delTodo={ this.delTodo }
-                     />
+            <div className="container">
+                <Header></Header>
+                <AddTodo addTodo={ this.addTodo }></AddTodo>
+                <Todos todos={this.state.todos}
+                       markComplete={ this.markComplete }
+                       delTodo={ this.delTodo }
+                       />
+            </div>
           </div>
       );
   }
